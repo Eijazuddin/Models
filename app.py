eb8d9d069065909e9e0568895b14cc861042b1fc
Eijazuddin <160104670+Eijazuddin@users.noreply.github.com>
2024-02-27 12:42:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+import pandas as pd
+import joblib
+
+st.title("Stress Model")
+
+data = pd.read_excel("C:/Users/eijaz/OneDrive/Desktop/stress_data.xlsx")
+
+posts = st.text_area("Enter your concern")
+
+if st.button("Predict"):
+    vector = joblib.load("stress.h5")
+   
+    predictions = vector.transform([posts])
+
+    model = joblib.load("stress_model.h5")
+
+    pred = model.predict(predictions)
+
+    st.success(pred)
+
+# We need not use conditional statements as its in labels and we didn;t encode it
+    
